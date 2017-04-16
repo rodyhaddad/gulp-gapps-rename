d0916da834c3d1aa1aad44ae50deb509d98f2d53
rodyhaddad <rody@rodyhaddad.com>
2017-04-16 15:37:30 +0300

    wip

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# gulp-gapps-rename
+# gulp-gapps-rename [![Build Status](https://travis-ci.org/rodyhaddad/gulp-gapps-rename.svg?branch=master)](https://travis-ci.org/rodyhaddad/gulp-gapps-rename)
+
+> My sublime gulp plugin
+
+
+## Install
+
+```
+$ npm install --save-dev gulp-gapps-rename
+```
+
+
+## Usage
+
+```js
+const gulp = require('gulp');
+const gappsRename = require('gulp-gapps-rename');
+
+gulp.task('default', () => {
+	gulp.src('src/file.ext')
+		.pipe(gappsRename())
+		.pipe(gulp.dest('dist'))
+);
+```
+
+
+## API
+
+### gappsRename([options])
+
+#### options
+
+##### foo
+
+Type: `boolean`<br>
+Default: `false`
+
+Lorem ipsum.
+
+
+## License
+
+MIT © [rodyhaddad](http://rodyhaddad.com)
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+var gutil = require('gulp-util');
+var through = require('through2');
+var path = require('path');
+
+module.exports = function (opts) {
+  opts = opts || {};
+
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      cb(null, file);
+      return;
+    }
+
+    if (file.isStream()) {
+      cb(new gutil.PluginError('gulp-gapps-rename', 'Streaming not supported'));
+      return;
+    }
+
+    try {
+      var basename = path.basename(file.path);
+      var extname = path.extname(file.path);
+      if (basename.match(/\.gs\.js$/)) {
+        file.path = path.dirname(file.path) + '/' + path.basename(basename, extname);
+      } else if (extname === '.js') {
+        file.path = path.dirname(file.path) + '/' + path.basename(basename, extname) + '.js.html';
+        file.contents = new Buffer('<script>\n' + file.contents.toString() + '\n</script>', opts);
+      } else if (extname === '.css') {
+        file.path = path.dirname(file.path) + '/' + path.basename(basename, extname) + '.css.html';
+        file.contents = new Buffer('<style>\n' + file.contents.toString() + '\n</style>', opts);
+      }
+
+      cb(null, file);
+    } catch (err) {
+      this.emit('error', new gutil.PluginError('gulp-gapps-rename', err));
+    }
+  }, function () {
+    console.log(arguments[0].toString());
+  });
+};
